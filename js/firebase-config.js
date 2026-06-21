3836727e3a0df0a5ba3311d3a448e55042bc4276
Karthick Raja P <karthickraja23122004@gmail.com>
2026-06-21 12:54:54 +0530

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,15 @@
+// Sentinel Dashboard - Firebase Configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvUhJcB20lwo3qvOFM0lAopwRVNOMWVX4",
+  authDomain: "my-sentinel-5e573.firebaseapp.com",
+  databaseURL: "https://my-sentinel-5e573-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "my-sentinel-5e573",
+  storageBucket: "my-sentinel-5e573.firebasestorage.app",
+  messagingSenderId: "734370576144",
+  appId: "1:734370576144:web:a42aed0c6e977dcf891795"
+};
+
+function isFirebaseConfigured() {
+  return true;
+}
